2ae9c9e7b8b31e75bb9f91f9834a2a2e7cd1a9e2
Trieu Vo <vantrieuvt89@gmail.com>
2021-04-08 15:40:47 +0700

    innit commit

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+// import firebase from 'firebase';
+// import 'firebase/messaging';
+// import {firebaseCloudMessagingConfig} from '../src/constants';
+
+// firebase.initializeApp({
+//   PROJECT_ID: firebaseCloudMessagingConfig.PROJECT_ID,
+//   SENDER_ID: firebaseCloudMessagingConfig.SENDER_ID,
+//   APP_ID: firebaseCloudMessagingConfig.APP_ID,
+// });
